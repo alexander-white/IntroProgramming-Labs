724898e3b46f78067970e8851b4b040e30aa6608
alex white <alexander.white2@marist.edu>
2018-09-13 15:17:02 -0400

    seperated the functions to improve quality!

--- madlib.py
+++ madlib.py
@@ -1,6 +1,15 @@
-n=input("Enter a noun: ")
-v=input("Enter a verb: ")
-a=input("Enter an adjective: ")
-p=input("Enter a place: ")
+def fillinblank():
+    global a, v, n, p
+    n=input("Enter a noun: ")
+    v=input("Enter a verb: ")
+    a=input("Enter an adjective: ")
+    p=input("Enter a place: ")
 
-print("\nTake your "+a+" "+n+" and "+v+" it at the "+p+"!")
+def madlibs():
+    print("\nTake your "+a+" "+n+" and "+v+" it at the "+p+"!")
+
+def main():
+    fillinblank()
+    madlibs()
+
+main()
