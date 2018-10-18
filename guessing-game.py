583cbbcdabc92d99e7188278c907d0a5bb983361
alex white <alexander.white2@marist.edu>
2018-10-18 15:06:35 -0400

    Usability improvements to game, resoving issues 4 and 5

--- guessing-game.py
+++ guessing-game.py
@@ -5,12 +5,17 @@ def main():
     answer="sheep"
     while answer == "sheep":
         print("I'm thinking of an animal.")
-        guess=input("Which animal is it? if you are done enter \"quit\"")
+        guess=input("Which animal is it? if you are done enter a word starting with\"q\"")
         guess=guess.lower()
         if guess == answer:
             print("congratulations,you win.")
+            intrest=input("do you like this animal? y or n")
+            if intrest[0] == "y":
+                print("that's great")
+            elif intrest[0] == "n":
+                print("not fond of them eh?")
             break
-        elif guess == "quit":
+        elif guess[0] == "q":
             break
         else:
             print("nope, try again please")
