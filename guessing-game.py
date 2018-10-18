ed2317c9ad6b0ea438b77bfbe5d62fc8317e2bda
alex white <alexander.white2@marist.edu>
2018-10-18 14:53:13 -0400

    fixed issues 2 and 3

--- guessing-game.py
+++ guessing-game.py
@@ -5,9 +5,12 @@ def main():
     answer="sheep"
     while answer == "sheep":
         print("I'm thinking of an animal.")
-        guess=input("Which animal is it?")
+        guess=input("Which animal is it? if you are done enter \"quit\"")
+        guess=guess.lower()
         if guess == answer:
             print("congratulations,you win.")
+            break
+        elif guess == "quit":
             break
         else:
             print("nope, try again please")
